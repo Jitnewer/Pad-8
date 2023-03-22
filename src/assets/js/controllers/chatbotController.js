ee469b1c566d63a0c876706c547123f3cf44c756
Kevin_Isaza <kevin.isaza.arias@hva.nl>
2023-03-22 16:39:08 +0100

    chatbox werkt niet volledig moet nog het database fixen

--- src/assets/js/controllers/chatbotController.js
+++ src/assets/js/controllers/chatbotController.js
@@ -15,6 +15,64 @@ export class ChatbotController extends Controller {
         this.#setupView();
     }
 
+//     async #setupView() {
+//         // Load the chatbox view
+//         const view = (this.#chatboxView = await this.loadHtmlIntoCustomElement(
+//             "html_views/chatbot.html",
+//             document.querySelector(".chatbox")
+//         ));
+//
+//         // Get the chatbot button and window elements
+//         const openForm = view.querySelector("#openForm");
+//         console.log("openForm:", openForm);
+//         const closeChatButton = view.querySelector("#closeChatButton");
+//         console.log("closeChatButton:", closeChatButton);
+//         const chatPopup = view.querySelector("#chatPopup");
+//         console.log("chatPopup:", chatPopup);
+//
+//
+//         // Add an event listener to the chatbot button
+//         openForm.addEventListener("click", () => {
+//             chatPopup.style.display = "block";
+//         });
+//
+//         // Add an event listener to the close button
+//         closeChatButton.addEventListener("click", () => {
+//             chatPopup.style.display = "none";
+//         });
+//
+//         // Create a new div element to hold the chatbot window
+//         const chatboxDiv = document.createElement("div");
+//         chatboxDiv.classList.add("chatbox");
+//
+//         // Append the chatbot window to the new div element
+//         // chatboxDiv.appendChild(view.body.firstChild);
+//
+// // Define the URL of the server endpoint that will return the chatbot data
+//         const url = '/chatbot';
+//
+// // Fetch the data from the server using the GET method
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(data => {
+//                 const list = document.querySelector('.chatbot-list');
+//
+//                 data.forEach(chat => {
+//                     const listItem = document.createElement('li');
+//                     listItem.innerText = `id: ${chat.id}, Question: ${chat.question}, Answer: ${chat.answer}`;
+//                     list.appendChild(listItem);
+//                 });
+//             })
+//             .catch(error => console.error(error));
+//
+//
+//
+//         // Add the new div element to the main content
+//         const mainContent = document.querySelector(".content");
+//         mainContent.appendChild(chatboxDiv);
+//     }
+// }
+
     async #setupView() {
         // Load the chatbox view
         const view = (this.#chatboxView = await this.loadHtmlIntoCustomElement(
@@ -29,8 +87,6 @@ export class ChatbotController extends Controller {
         console.log("closeChatButton:", closeChatButton);
         const chatPopup = view.querySelector("#chatPopup");
         console.log("chatPopup:", chatPopup);
-        const chatTextarea = view.querySelector("#chatTextarea");
-        console.log("chatTextarea:", chatTextarea);
 
         // Add an event listener to the chatbot button
         openForm.addEventListener("click", () => {
@@ -47,10 +103,30 @@ export class ChatbotController extends Controller {
         chatboxDiv.classList.add("chatbox");
 
         // Append the chatbot window to the new div element
-        chatboxDiv.appendChild(view.body.firstChild);
+        // chatboxDiv.appendChild(view.body.firstChild);
+
+        // Define the URL of the server endpoint that will return the chatbot data
+        const url = "/chatbot";
+
+        // Fetch the data from the server using the GET method
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                // Loop through the chatbot data and append each question and answer to a list item
+                const list = document.querySelector(".chatbot-list");
+
+                data.forEach((chat) => {
+                    const listItem = document.createElement("li");
+                    listItem.innerText = `id: ${chat.id},Question: ${chat.question}, Answer: ${chat.answer}`;
+                    list.appendChild(listItem);
+                });
+            })
+            .catch((error) => console.error(error));
 
         // Add the new div element to the main content
         const mainContent = document.querySelector(".content");
         mainContent.appendChild(chatboxDiv);
     }
+
+
 }
