fdd2e58b268b3ec2f40dd5895eea83dea04f911e
Chant Balci <Chant.balci@hva.nl>
2023-03-25 21:29:10 +0100

    de delete optie in the admin proefles is klaar

--- src/assets/js/repositories/adminDashboardTrialLessonRepository.js
+++ src/assets/js/repositories/adminDashboardTrialLessonRepository.js
@@ -7,17 +7,16 @@ import {NetworkManager} from "../framework/utils/networkManager.js";
 export class AdminDashboardTrialLessonRepository {
     #networkManager;
     #route;
-constructor() {
-    this.#route = "/adminDashboard";
-    this.#networkManager = new NetworkManager();
-}
+    constructor() {
+        this.#route = "/adminDashboard";
+        this.#networkManager = new NetworkManager();
+    }
     saveTestlesson(name, timeDuration, date, location, room, subject, time){
         console.log(this.#route);
         return this.#networkManager.doRequest(this.#route, "POST",
             {name: name,Admin_idAdmin:1, timeDuration: timeDuration, date: date, location: location, room: room, subject: subject, time: time})
- }
-    // getTestlesson(name){
-    //     console.log(this.#route);
-    //     return this.#networkManager.doRequest(`${this.#route}/${name}`, "GET");
-    // }
+    }
+    deleteTestlesson(id) {
+        return this.#networkManager.doRequest(`${this.#route}/${id}`, "DELETE");
+    }
 }
